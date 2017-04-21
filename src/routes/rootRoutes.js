d69783e7347bc1f9a78e7401ed9c701f8a6b74be
yamapi06150 <yamapi06150@gmail.com>
2017-04-21 22:38:00 +0800

    using jwt to generate signedToken in order to achieve mutual trust

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -1,4 +1,12 @@
 const User = require('./../models/user');
+const jwt = require('jsonwebtoken');
+const secret = require('./../config/config').secret;
+
+const genToken = (user) => {
+    return jwt.sign(user, secret, {
+        expiresIn: 3000
+    });
+ } ;
 
 module.exports = (app) => {
     app.post('/auth/login', function (req, res) {
@@ -9,6 +17,7 @@ module.exports = (app) => {
                 if (err) { return console.log(err); }
                 if (!isMatch) { return res.status(403).json({ error: "密碼無效~~" }); }
                 return res.json({
+                    token: genToken( {name: user.username} ),
                     user: { name: user.username }
                 });
             });
