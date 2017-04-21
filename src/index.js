29749027efa177e60596d201b740c19cb525c3ff
yamapi06150 <yamapi06150@gmail.com>
2017-04-21 21:38:01 +0800

    mongo db setup, connection ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const port = require('./config/config').port;
 const rootRoutes = require('./routes/rootRoutes');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dbConnectionString = require('./config/config').dbConnectionString;
+
+console.log(dbConnectionString);
+mongoose.connect(dbConnectionString);
+const db = mongoose.connection;
+db.on('error', (err) => { console.log('connection failed!', err); } );
+db.once('open', () => { console.log('success'); } );
 
 app.use(bodyParser.urlencoded( {extended: false } ));
 app.use(bodyParser.json());
