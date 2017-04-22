bd7c01ed4eea4112130b78111d9d4bd0b2a42b95
yamapi06150 <yamapi06150@gmail.com>
2017-04-22 15:38:12 +0800

    complete posts api with server side auth

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -7,7 +7,32 @@ const genToken = (user) => {
     return jwt.sign(user, secret, {
         expiresIn: 3000
     });
- } ;
+};
+
+const requireAuth = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret, function (err, decoded) {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    return res.status(401).json({ error: 'token失效~~你要重新登入喔！！' });
+                } else {
+                    return res.status(401).json({ error: '認證失敗' });
+                }
+            } else {
+                if (decoded.admin === true) {
+                    next();
+                } else {
+                    res.status(401).json({ error: '認證失敗..' });
+                }
+            }
+        });
+    } else {
+        return res.status(403).json({
+            error: '請提供token yo~~'
+        });
+    }
+}
 
 module.exports = (app) => {
     app.post('/auth/login', function (req, res) {
@@ -19,33 +44,33 @@ module.exports = (app) => {
                 if (err) { return console.log(err); }
                 if (!isMatch) { return res.status(403).json({ error: "密碼無效~~" }); }
                 return res.json({
-                    token: genToken( {name: user.username, admin: user.admin} ),
+                    token: genToken({ name: user.username, admin: user.admin }),
                     user: { name: user.username, admin: user.admin }
                 });
             });
         });
-    } ) ;
+    });
     app.post('/auth/signup', async (req, res) => {
         let user = new User();
-        user.username = req.body.username ;
-        user.password = req.body.password ;
-        try{
+        user.username = req.body.username;
+        user.password = req.body.password;
+        try {
             await user.save();
             return res.json({
-                token: genToken( { name: user.username } ),
-                user: {name: user.username}
+                token: genToken({ name: user.username }),
+                user: { name: user.username }
             });
-        }catch(e){
+        } catch (e) {
             return console.log('oops...', e);
         }
-    } );
-    app.post('/posts', async function(req, res){
+    });
+    app.post('/posts', requireAuth, async function (req, res) {
         let post = new Post();
-        post.name = req.body.name ;
-        post.content = req.body.content ;
+        post.name = req.body.name;
+        post.content = req.body.content;
         try {
             await post.save();
-        }catch(e){
+        } catch (e) {
             console.log(e);
         }
         res.json({
