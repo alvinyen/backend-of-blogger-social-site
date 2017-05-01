4152e01ce92decd6436bc1dba9b81b7e31135074
yamapi06150 <yamapi06150@gmail.com>
2017-05-02 05:37:37 +0800

    complete edit and delete

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -63,7 +63,7 @@ module.exports = (app) => {
         } catch (e) {
             if (e.name === 'MongoError' && e.code === 11000) {
                 // Duplicate username
-                console.log('User already exist!') ;
+                console.log('User already exist!');
                 return res.status(500).send({ errorMsg: 'User already exist!' });
             }
             return console.log(e);
@@ -87,6 +87,7 @@ module.exports = (app) => {
         });
     });
     app.get('/posts', function (req, res) {
+        console.log('order');
         Post.find({}, 'name', function (err, posts) {
             if (err) return console.log(err);
             res.json({
@@ -94,5 +95,50 @@ module.exports = (app) => {
                 message: '你得到所有文章了呢！！'
             });
         })
-    })
+    });
+    app.get('/posts/:post_id', function (req, res) {
+        Post.findById({ _id: req.params.post_id }, function (err, post) {
+            if (err) return res.status(500).json({ error: err.message });
+            // 422 ???
+            res.json({ post });
+        })
+    });
+    app.put('/posts/:post_id', requireAuth, function (req, res) {
+        console.log(req.body);
+        console.log(`req.body.name: ${req.body.name}`);
+        console.log(`req.body.content: ${req.body.content}`);
+        console.log(`req.params.post_id: ${req.params.post_id}`);
+        Post.findById({ _id: req.params.post_id }, function (err, post) {
+            if (err) return res.status(500).json({ error: err.message });
+            // console.log();
+            // console.log(`origin name: ${post.name}`);
+            // console.log(`origin content: ${post.content}`);
+
+            post.name = req.body.name;
+            post.content = req.body.content;
+
+            post.save(function (err) {
+                if (err) return res.status(500).json({ error: err.message });
+                res.json({
+                    post: post,
+                    message: '文章更新成功了！'
+                });
+            });
+        });
+    });
+    app.delete('/posts/:post_id', requireAuth, function (req, res) {
+        if(req.params.post_id === undefined || req.params.post_id === 'undefined') res.status(400).json({ error: `post_id === undefined` });
+        const id = req.params.post_id;
+        console.log(req.params.post_id);
+        Post.findById({ _id: id }, function (err, post) {
+            post.remove(function (err) {
+                if (err) return res.status(422).json({ error: err.message });
+                res.json({
+                    _id: id,
+                    message: '文章移除成功！'
+                });
+            });
+        });
+    });
+
 }
