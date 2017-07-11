7e99247a76c5608ea34cbf7fe08c196f2743e798
yamapi06150 <yamapi06150@gmail.com>
2017-07-11 23:26:38 +0800

    test initial hook

--- src/models/post.js
+++ src/models/post.js
@@ -17,7 +17,7 @@ const postSchema = new Schema(
                       // arrow function會自動綁定外層的this，且綁了就不能再改..
                       // 這邊要的是動態的 this，也就是post/document entiry
                       // 使用arrow function千萬要看時機...不要習慣了就全都用啊！！
-postSchema.pre('save', function (next) {
+postSchema.pre('init', function (next) {
   CouterModel.findByIdAndUpdate({ _id: 'postId' }, { $inc: { seq: 1 } }, { new: true, upsert: true }, (err, counter) => {
     if (err) { console.log('postSchema pre save  error', err); return next(err); }
     this.postId = counter.seq;
